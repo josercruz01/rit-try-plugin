50d26ebbe896d8b18a3efbf65c0b14ef6f6441fe
Wander <wander.bravo@gmail.com>
2013-04-27 15:16:04 -0400

    created the 3 sections and the ui of the plugin

--- ui/com.plugin.tryplugin/src/com/plugin/tryplugin/views/SampleView.java
+++ ui/com.plugin.tryplugin/src/com/plugin/tryplugin/views/SampleView.java
@@ -6,11 +6,17 @@ import java.util.ArrayList;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
 import org.eclipse.ui.forms.widgets.Form;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Hyperlink;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
+import org.eclipse.ui.forms.widgets.TableWrapData;
+import org.eclipse.ui.forms.widgets.TableWrapLayout;
 import org.eclipse.ui.part.*;
 	
 import org.eclipse.jface.viewers.*;
@@ -30,6 +36,8 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.text.PasswordView;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
@@ -61,12 +69,18 @@ public class SampleView extends ViewPart {
 
 	private TreeViewer viewer;
 	private DrillDownAdapter drillDownAdapter;
+	
+	private ScrolledForm form;
+	
 	private Action action1;
-	//private Action action2;
+	private Action action2;
 	private Action doubleClickAction;
+	private Action doubleClickAction2;
+	
 	private TableViewer table;
+	private ArrayList<String> tryElement = new ArrayList<String>();
 	
-	
+	private String tryStatus="";
 	/*
 	 * The content provider class is responsible for
 	 * providing objects to the view. It can wrap
@@ -238,7 +252,7 @@ public class SampleView extends ViewPart {
 	    //ScrolledForm form = toolkit.createScrolledForm(parent);
 	    //form.setText(" ScrolledForm Text");
 		
-		
+		/*Section 1*/
 		Section section2 = toolkit.createSection(parent, Section.DESCRIPTION
 		        | Section.TITLE_BAR);
 	    section2.setText("Files Local Machine"); //$NON-NLS-1$
@@ -275,17 +289,23 @@ public class SampleView extends ViewPart {
 		
 		hookContextMenu();
 		
+
+		
 		hookDoubleClickAction();
 		
 		contributeToActionBars();
 		section2.setClient(browseLocal);
 		
-		
+		/*Section 1*/
+	    
+	    
+	    /*Section 2*/
 	    // Lets make a layout for the first section of the screen
 	    GridLayout layout = new GridLayout();
 	    layout.numColumns = 1;
 	    layout.marginWidth = 2;
 	    layout.marginHeight = 2;
+	    
 	    // Creating the Screen
 	    Section section = toolkit.createSection(parent, Section.DESCRIPTION
 	        | Section.TITLE_BAR);
@@ -303,7 +323,7 @@ public class SampleView extends ViewPart {
 	    
 	    GridData gd = new GridData(GridData.FILL_BOTH);
 	    gd.heightHint = 20;
-	    gd.widthHint = 100;
+	    gd.widthHint = 200;
 	    
 	    t.setLayoutData(gd);
 	    toolkit.paintBordersFor(client);
@@ -332,10 +352,134 @@ public class SampleView extends ViewPart {
 
 	      public Image getImage(Object element) {
 	        return PlatformUI.getWorkbench().getSharedImages()
-	            .getImage(ISharedImages.IMG_OBJ_ELEMENT);
+	            .getImage(ISharedImages.IMG_OBJ_FILE);
 	      };
 	    });
-	    table.setInput(getViewSite());
+	    
+	    makeActions2();
+		hookContextMenu2();
+		hookDoubleClickAction2();
+		
+	    getSite().setSelectionProvider(table);
+	    table.setInput(tryElement);
+	    
+	    /*Section 2*/
+	    
+	    
+	    
+		/*Section 3*/
+	    toolkit = new FormToolkit(parent.getDisplay());
+	    form = toolkit.createScrolledForm(parent);
+	    form.setText("Configure the Connection to the Server");
+	    GridLayout layout3 = new GridLayout();
+	    form.getBody().setLayout(layout3);
+	    
+	    
+	    layout3.numColumns = 2;
+	    GridData gd3 = new GridData();
+	    gd3.horizontalSpan = 2;
+	    
+	    
+	    Label labelServer = new Label(form.getBody(), SWT.NULL);
+	    labelServer.setText("Server Name:");
+	    
+	    Text text = new Text(form.getBody(), SWT.BORDER);
+	    text.setTextLimit(20);
+	    text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	    
+	    Label labelUser = new Label(form.getBody(), SWT.NULL);
+	    labelUser.setText("User Name:");
+	    
+	    Text textUser = new Text(form.getBody(), SWT.BORDER);
+	    textUser.setTextLimit(10);
+	    textUser.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	    
+	    Label labelPassword = new Label(form.getBody(), SWT.NULL);
+	    labelPassword.setText("Password:");	        
+	     
+	    Text textPassword = new Text(form.getBody(), SWT.BORDER);
+	    textPassword.setTextLimit(10);
+	    textPassword.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	    
+	    Label labelInstructor = new Label(form.getBody(), SWT.NULL);
+	    labelInstructor.setText("Instructor account:");
+	    
+	    Text textInstructr = new Text(form.getBody(), SWT.BORDER);
+	    textInstructr.setTextLimit(15);
+	    textInstructr.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	    
+	    Label labelAssignment = new Label(form.getBody(), SWT.NULL);
+	    labelAssignment.setText("Assignment code:");
+	    
+	    Text textAssignment = new Text(form.getBody(), SWT.BORDER);
+	    textAssignment.setTextLimit(10);
+	    textAssignment.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	    
+	    Button button = new Button(form.getBody(), SWT.CHECK);
+	    
+	    button.setText("Save the files in the server");
+	    gd3 = new GridData();
+	    gd3.horizontalSpan = 2;
+	    button.setLayoutData(gd3);
+	    
+	    Label SaveFile = new Label(form.getBody(), SWT.NULL);
+	    SaveFile.setText("Do you want to see the output File: ");
+	    
+	    Hyperlink link = toolkit.createHyperlink(form.getBody(), 
+	  	      "Click here.", SWT.WRAP);
+	  	    link.addHyperlinkListener(new HyperlinkAdapter() {
+	  	     public void linkActivated(HyperlinkEvent e) {
+	  	      System.out.println("Link activated!");
+	  	     }
+	  	    });
+	  	  link.setLayoutData(gd3);
+	  	  
+	  	  Label OutputTry = new Label(form.getBody(), SWT.NULL);
+		    SaveFile.setText("try "+textInstructr.getText()+" "+textAssignment.getText()+ tryStatus);
+		    //try instructor-account assignment-code file-list
+	    /*
+		Section section3 = toolkit.createSection(parent, Section.DESCRIPTION
+		        | Section.TITLE_BAR);
+		section3.setText("Connection Setup"); //$NON-NLS-1$
+		section3.setDescription("Setting up the connection");
+	    
+	    Composite browseLocal2 = toolkit.createComposite(section3, SWT.WRAP);
+	    
+    	//viewer = new TreeViewer(browseLocal2, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+	    
+    	GridLayout layout3 = new GridLayout();
+    	layout3.numColumns = 1;
+    	layout3.marginWidth = 2;
+    	layout3.marginHeight = 2;
+	    
+	    //viewer.
+	    browseLocal2.setLayout(layout3);
+	    
+	    form = toolkit.createScrolledForm(browseLocal2);  	    
+	    
+	    form.setText("Hello, Eclipse Forms");
+	    
+	    //TableWrapLayout layoutWrap = new TableWrapLayout();
+	    form.getBody().setLayout(layout3);
+	    
+	    
+	    
+	    //link.setText("This is an example of a form that is much longer and will need to wrap.");
+	    layout.numColumns = 2;
+	    TableWrapData td = new TableWrapData();
+	    td.colspan = 2;
+	    //link.setLayoutData(td);
+	    Label label = toolkit.createLabel(form.getBody(), "Text field label:");
+	    Text text = toolkit.createText(form.getBody(), "");
+	    td = new TableWrapData(TableWrapData.FILL_GRAB);
+	    text.setLayoutData(td);
+	    Button button = toolkit.createButton(form.getBody(), "A checkbox in a form", SWT.CHECK);
+	    td = new TableWrapData();
+	    td.colspan = 2;
+	    button.setLayoutData(td);
+	    
+	    */
+	    /*Section 3*/
 	    
 	}
 
@@ -352,6 +496,19 @@ public class SampleView extends ViewPart {
 		getSite().registerContextMenu(menuMgr, viewer);
 	}
 
+	private void hookContextMenu2() {
+		MenuManager menuMgr = new MenuManager("#2ndMenu");
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				SampleView.this.fillContextMenu(manager);
+			}
+		});
+		Menu menu = menuMgr.createContextMenu(table.getControl());
+		table.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuMgr, table);
+	}
+	
 	private void contributeToActionBars() {
 		IActionBars bars = getViewSite().getActionBars();
 		fillLocalPullDown(bars.getMenuManager());
@@ -361,12 +518,12 @@ public class SampleView extends ViewPart {
 	private void fillLocalPullDown(IMenuManager manager) {
 		manager.add(action1);
 		manager.add(new Separator());
-		//manager.add(action2);
+		manager.add(action2);
 	}
 
 	private void fillContextMenu(IMenuManager manager) {
 		manager.add(action1);
-		//manager.add(action2);
+		manager.add(action2);
 		manager.add(new Separator());
 		drillDownAdapter.addNavigationActions(manager);
 		// Other plug-ins can contribute there actions here
@@ -376,7 +533,7 @@ public class SampleView extends ViewPart {
 	private void fillLocalToolBar(IToolBarManager manager) {
 		
 		manager.add(action1);
-		//manager.add(action2);
+		manager.add(action2);
 		manager.add(new Separator());
 		
 		drillDownAdapter.addNavigationActions(manager);
@@ -394,7 +551,14 @@ public class SampleView extends ViewPart {
 				Object obj = ((IStructuredSelection)selection).getFirstElement();
 				File tmp = (File)obj;								
 				if(!tmp.isDirectory())
-					showMessage("Trying to upload this file: "+obj.toString());
+				{	
+					//showMessage("Trying to upload this file: "+obj.toString());
+					if(!tryElement.contains(obj.toString()))
+					{	
+						tryElement.add(obj.toString());						
+						table.setInput(tryElement);
+					}
+				}
 			}
 		};
 		//showMessage("Action 1 executed");
@@ -404,21 +568,32 @@ public class SampleView extends ViewPart {
 		//path1 = ""+ obj.toString();
 		
 		action1.setText("Upload this");
-		action1.setToolTipText("Action 1 tooltip");
+		action1.setToolTipText("Add this file to the list");
 		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
 			getImageDescriptor(ISharedImages.IMG_OBJ_ADD));
 		
-		/*
 		action2 = new Action() {
 			public void run() {
-				showMessage("Action 2 executed");
+				//showMessage("Action 2 executed");
+				ISelection selection = viewer.getSelection();
+				Object obj = ((IStructuredSelection)selection).getFirstElement();
+				File tmp = (File)obj;								
+				if(!tmp.isDirectory())
+				{	
+					//showMessage("Trying to upload this file: "+obj.toString());
+					if(tryElement.contains(obj.toString()))
+					{	
+						tryElement.remove(obj.toString());						
+						table.setInput(tryElement);
+					}
+				}
 			}
 		};
-		action2.setText("Action 2");
-		action2.setToolTipText("Action 2 tooltip");
+		action2.setText("Remove this");
+		action2.setToolTipText("Remove this file from the list");
 		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		*/
+				getImageDescriptor(ISharedImages.IMG_OBJS_ERROR_TSK));
+
 		
 		doubleClickAction = new Action() {
 			public void run() {
@@ -428,11 +603,42 @@ public class SampleView extends ViewPart {
 				File tmp = (File)obj;				
 				
 				if(!tmp.isDirectory())
-					showMessage("Trying to upload "+obj.toString());
+					{
+						//showMessage("Trying to upload "+obj.toString());
+						if(!tryElement.contains(obj.toString()))
+						{	
+							tryElement.add(obj.toString());						
+							table.setInput(tryElement);
+						}
+					}
 			}
 		};
+		
+		doubleClickAction2 = new Action() {
+			public void run() {
+				
+				ISelection selection = table.getSelection();
+				Object obj = ((IStructuredSelection)selection).getFirstElement();
+				String tmp = (String)obj;				
+								
+				//showMessage("Trying to upload "+obj.toString());
+				if(tryElement.contains(tmp.toString()))
+				{	
+					tryElement.remove(tmp.toString());						
+					table.setInput(tryElement);
+				}
+				
+			}
+		};
+		
 	}
 
+	private void makeActions2() {
+		
+		
+		
+	
+	}
 	private void hookDoubleClickAction() {
 		
 		viewer.addDoubleClickListener(new IDoubleClickListener() {
@@ -441,6 +647,16 @@ public class SampleView extends ViewPart {
 			}
 		});
 	}
+	private void hookDoubleClickAction2() {
+		
+		table.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent event) {
+				doubleClickAction2.run();
+			}
+		});
+	}
+	
+	
 	private void showMessage(String message) {
 		MessageDialog.openInformation(
 			viewer.getControl().getShell(),
